50c64961108cd612ad5607bbff7c24c852b441a1
Teddy VALETTE <teddy.valette1@etu.univ-lorraine.fr>
2017-03-07 17:21:46 +0100

    Ajout des fichiers de bases

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\App;
+
+$app->get("/", function (Request $request, Response $response){
+$response->getBody()->write("Hello World =)");
+});
+
+$app->run();
+
