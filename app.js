dcf6fe7996d769308474e4265b6f30199c225b1e
Romain <romain.kraft3@etu.univ-lorraine.fr>
2017-03-10 22:10:07 +0100

    Ajout des covers

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/', function(req, res) {
         html+="<div class='item'><div class='vignette'><img src='"+musicUpload[i].cover+"' alt='"+musicUpload[i].track+"'/></div></div>";
     }
     console.log(html);
-    res.render('index', { vignettes : html});
+    res.render('index', { vignettes : html,chat: chat});
 });
 
 app.get('/musics',function (req,res) {
