b40551b041a809a8e5c4d3ad9be6d74e216f99bb
Alexis Barthelemy <alexis.barthelemy8@etu.univ-lorraine.fr>
2017-03-11 00:32:57 +0100

    Ajout du chat dans la fenetre principale

---
+++ include/client.js
@@ -0,0 +1,41 @@
+(function () {
+
+	var socket = io.connect("http://localhost:3003");
+	var msgtpl = $('#msgtpl').html();
+	$('#msgtpl').remove();
+
+	$('#loginform').submit(function (event) {
+		event.preventDefault();
+		socket.emit('login', {
+			username : $('#username').val(),
+		});
+	});
+
+	socket.on('loged' , function () {
+		$("#login").fadeOut();
+	})
+
+	//envois de message 
+	$('#form').submit(function (event) {
+		event.preventDefault();
+		socket.emit('newmsg', {message : $('#message').val()});
+		$('#message').val('');
+		$('#message').focus();
+	})
+
+	socket.on('newmsg', function (message) {
+		$('#messages').append('<div class="message">'+ Mustache.render(msgtpl,message) + '</div>');
+		$('#messages').animate({scrollTop : $('#messages').prop('scrollHeight')} , 500);
+	});
+
+	//gestion des connection
+	socket.on('newuser', function (user) {
+		$("#users").append('<p>' + user.username + '</p>');
+	});
+
+	//gestion des deconnections
+	socket.on('discuser', function (user) {
+		$('#' + user.id).remove();
+	})
+
+})();
