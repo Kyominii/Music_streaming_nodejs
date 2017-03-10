dae774f5efd0040eae9144db6221059a89b8f49e
Teddy VALETTE <teddyche@hotmail.Fr>
2017-03-10 17:15:00 +0100

    Ajout des dépendances icecast et lame

---
+++ server.js
@@ -0,0 +1,26 @@
+var icecast = require("icecast"), // I'll talk about this module later
+    lame = require("lame");
+
+var encoder = lame.Encoder({channels: 2, bitDepth: 16, sampleRate: 44100});
+encoder.on("data", function(data) {
+    sendData(data);
+});
+var decoder = lame.Decoder();
+decoder.on('format', function(format) {
+    decoder.pipe(encoder);
+});
+
+var url = 'http://voxystudio.com:25567'
+icecast.get(url, function(res) {
+    res.on('data', function(data) {
+        decoder.write(data);
+    });
+}
+
+var clients = []; // consider that clients are pushed to this array when they connect
+
+function sendData(data){
+    clients.forEach(function(client) {
+        client.write(data);
+    });
+}
