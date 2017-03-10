825da88945a571b497d06300ed7aa7fc53a84385
Romain <romain.kraft3@etu.univ-lorraine.fr>
2017-03-10 18:19:59 +0100

    On recupere les metadata de spotify si dispo

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const mm = require('musicmetadata');
+const SpotifyWebApi = require('spotify-web-api-node');
+const async = require('async');
 const app = express();
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+    clientId : '1b25b20eb6644b7f84861a486146b68a',
+    clientSecret : '159c9817efd544059cb01b2cdc5a3a19',
+    redirectUri : 'http://localhost/callback'
+});
+
+var spotifyMeta ='';
+var simpleMeta = '';
 
 // default options
 app.use(fileUpload());
@@ -20,20 +32,43 @@ app.post('/upload', function(req, res) {
 
     var musicUploaded = req.files.musique;
     var nomFichier = musicUploaded.name.split(".");
+    var uploadPath = __dirname + '/uploads/'+musicUploaded.name;
+
     if(nomFichier[nomFichier.length-1]=="mp3")
     {
         // Use the mv() method to place the file somewhere on your server
-        musicUploaded.mv(__dirname + '/uploads/'+musicUploaded.name, function(err) {
-
-            var parser = mm(fs.createReadStream(__dirname + '/uploads/'+musicUploaded.name), function (err, metadata) {
-                if (err) throw err;
-                return res.status(200).send(metadata);
-            });
-
-
-
+        musicUploaded.mv(uploadPath, function(err) {
             if (err) {
             return res.status(500).send(err);
+            }else
+            {
+                var calls = [];
+                calls.push(function(callback){
+
+                    //On doit passer en synchrone
+                    var parser = mm(fs.createReadStream(uploadPath), function (err, metadata) {
+                        if (err) throw err;
+                        simpleMeta = metadata;
+                        spotifyApi.searchTracks('track:'+metadata.title+' artist:'+metadata.artist)
+                            .then(function(data) {
+                                console.log(data.body);
+                                spotifyMeta = data.body;
+                                callback();
+                            }, function(err) {
+                                console.log('Something went wrong!', err);
+                                callback();
+                            });
+                    });
+                });
+                async.parallel(calls,function (err,result) {
+                    if(err)throw err;
+                    if(spotifyMeta.tracks.items[0] !== undefined) {
+                        return res.status(200).send(spotifyMeta.tracks.items[0]);
+                    }else{
+                        return res.status(200).send(simpleMeta);
+                    }
+                });
+
             }
     });
     }else{
