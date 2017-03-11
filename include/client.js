906a87a2037cd40f0a76475c745f6f491bfa02fd
AMSLER Ophélien <ophelien.amsler7@etu.univ-lorraine.fr>
2017-03-11 04:25:25 +0100

    debug envoi

--- include/client.js
+++ include/client.js
@@ -36,22 +36,40 @@
 	//gestion des deconnections
 	socket.on('discuser', function (user) {
 		$('#' + user.id).remove();
-	})
+	});
 
     //gestion des deconnections
     socket.on('newMusic', function (user) {
     	console.log(user);
 
-    	var el = $("<div>").toggleClass("vignette");
-    	var img = $("<img>").attr("src", user.cover).attr("alt", user.track);
-    	var a = $("a").attr("href", user.preview);
-    	var p = $("p").append(user.track);
-
-		el.append(img).append(a).append("Preview").append(p);
+		/*<div class='item' name='"+musicUpload[i].preview+"'>"+
+		 "<div class='vignette'>"+
+		 "<img src='"+musicUpload[i].cover+"' alt='"+musicUpload[i].track+"'/>"+
+		 "<div class='info'><div>"+
+		 "<div>"+musicUpload[i].album+"</div>"+
+		 "<div>"+musicUpload[i].artist+"</div>"+
+		 "<div>"+musicUpload[i].track+"</div>"+
+		 "</div>"+
+		 "</div>"+
+		 "</div></div>";*/
+		var item = $("<div>").addClass("item").attr("name",user.preview).append(
+			$("<div>").addClass("vignette").append(
+				$("<img>").attr("src", user.cover).attr("alt", user.track)
+			).append(
+				$("<div>").addClass("info").append(
+					$("<div>").append(
+						$("<div>").text(user.album)
+					).append(
+						$("<div>").text(user.artist)
+					).append(
+						$("<div>").text(user.track)
+					)
+				)
+			)
+		);
         $('.owl-carousel')
-            .owlCarousel('add', el)
+            .owlCarousel('add', item)
             .owlCarousel('update')
-
-    })
+    });
 
 })();
