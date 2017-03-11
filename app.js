c71e569f9f7d377077fcf6753f0cf19b7f0537af
Teddy VALETTE <teddyche@hotmail.Fr>
2017-03-11 03:47:05 +0100

    Ajout d'un console log

--- app.js
+++ app.js
@@ -145,9 +145,8 @@ app.post('/', function(req, res) {
                             var sys = require('sys')
                             var exec = require('child_process').exec;
                             var pid;
-                            function puts(error, stdout, stderr) { sys.puts(stdout); console.log(stdout); exec("kill -1 " + pid, puts3); }
-                            function puts2(error, stdout, stderr) { pid = stdout; console.log(pid); }
-                            function puts3(error, stdout, stderr) { sys.puts(stdout); console.log(stdout); }
+                            function puts2(error, stdout, stderr) { pid = stdout; console.log(pid); exec("kill -1 " + pid, puts3); console.log("kill -1 " + pid + "terminé"); }
+                            function puts3(error, stdout, stderr) { console.log(stdout); }
                             exec("find /home/hackathon/www/uploads/ -name *.mp3 > /home/hackathon/playlist.m3u", puts3);
                             exec("ps aux | grep ezstream | grep -v 'grep' | grep -o '[0-9]*' | head -n1", puts2);
                         }
